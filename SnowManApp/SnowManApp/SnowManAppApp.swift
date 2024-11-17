4b46657b0a99c0605f5ce20f6af5fa918c054a0c
Aldair Cosetito <aldair.loq.369@gmail.com>
2024-11-17 18:44:43 -0500

    feat(design): Initial commit

---
+++ SnowManApp/SnowManApp/SnowManAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowManAppApp.swift
+//  SnowManApp
+//
+//  Created by Aldair Cosetito Coral on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowManAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
